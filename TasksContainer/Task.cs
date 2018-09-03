c3f05a23f2f7cb6fd02aadee7245042d8a263005
Nicolas <nicolascabreraprogramador@gmail.com>
2018-09-03 12:10:37 -0300

    update LINQ samples

--- TasksContainer/Task.cs
+++ TasksContainer/Task.cs
@@ -28,26 +28,64 @@ namespace TasksContainer
         /// Carga una lista de objetos Person
         /// </summary>
         /// <param name="people">Lista instanciada de Person</param>
-        public static void LoadPeopleList(List<Person> people)
+        public static void LoadPeopleList(ref List<Person> people)
         {
+            ReadPeopleFromJsonFile(ref people);
+        }
 
-            Console.WriteLine("La lista se ha cargado exitosamente");
-            ReadItemsFromJsonFile(people);
+
+        /// <summary>
+        /// Lee una lista de people desde un archivo json
+        /// </summary>
+        public static void ReadPeopleFromJsonFile(ref List<Person> people) {
+            using (StreamReader r = new StreamReader(@"D:\Repositorios\2018\C#\TestConsoleApplication\data\people.json"))
+            {
+               string json = r.ReadToEnd();
+               people = JsonConvert.DeserializeObject<List<Person>>(json);
+            }
+        }
+
+
+        public static void SortByName(List<Person> people) {
+
+            var orderedPeople = people.OrderBy(p => p.name).ToList();
+
+
+            Console.WriteLine("Lista ordenada \n");
+            foreach (var item in orderedPeople)
+            {
+                Console.WriteLine(item.name);
+            }
+
+            Console.WriteLine("\nLista por defecto");
+            foreach (var item in people)
+            {
+                Console.WriteLine(item.name);
+            }
+
+            Console.Read();
+        }
+
+
+
+
+        public static void LoadItemsList(ref List<Item> items)
+        {
+            ReadItemsFromJsonFile(ref items);
         }
 
 
         /// <summary>
         /// Lee una lista de items desde un archivo json
         /// </summary>
-        public static dynamic ReadItemsFromJsonFile(List<Person> people) {
-            using (StreamReader r = new StreamReader(@"D:\Repositorios\2018\C#\TestConsoleApplication\data\people.json"))
+        public static void ReadItemsFromJsonFile(ref List<Item> items)
+        {
+            using (StreamReader r = new StreamReader(@"D:\Repositorios\2018\C#\TestConsoleApplication\data\listaDeItems.json"))
             {
-               string json = r.ReadToEnd();
-               people = JsonConvert.DeserializeObject<List<Person>>(json);
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<Item>>(json);
             }
-            return people;
         }
-        
     }
     
 }
--- TestConsoleApplication/Program.cs
+++ TestConsoleApplication/Program.cs
@@ -15,12 +15,34 @@ namespace TestConsoleApplication
         static void Main(string[] args)
         {
 
+            /*
             List<Person> people = TasksContainer.Task.CreatePersonList();
 
-            TasksContainer.Task.LoadPeopleList(people);
+            TasksContainer.Task.LoadPeopleList(ref people);
+
+            TasksContainer.Task.SortByName(people);
+            */
+
+            List<Item> items = new List<Item>(); 
+            TasksContainer.Task.LoadItemsList(ref items);
+
+
+            //Expresion de consulta/Sintaxis de consulta
+            var itemList1 = (from i in items
+                           where i.quantity == 2
+                           select i).ToList(); 
+
+
+            //Expresion de método(Sintaxis de método
+            var itemList2 = items.Where(i => i.quantity == 2).Select(i => i).ToList();
+
+
+            var range = Enumerable.Range(1, 100).Select(x => Math.Pow(x,2)).ToList();
+          
+
+            Console.ReadKey();
+
+
         }
-
-
-        
     }
 }
